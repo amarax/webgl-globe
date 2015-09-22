20c9d8f16e1b9afe0806e9331312087ae3c569cf
Yong Lin <yong.lin@spire.com>
2015-09-23 01:42:31 +0800

    Minor Visual Tweaks
    - Clamped camera target to something reasonable.
    - Adjusted atmosphere effect on globe mesh.

--- globe/globe.js
+++ globe/globe.js
@@ -51,7 +51,7 @@ DAT.Globe = function(container, opts) {
         'void main() {',
           'vec3 diffuse = texture2D( texture, vUv ).xyz;',
           'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
-          'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
+          'vec3 atmosphere = vec3( 0.5, 0.5, 0.5 ) * pow( intensity, 3.0 );',
           'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
         '}'
       ].join('\n')
@@ -357,6 +357,7 @@ DAT.Globe = function(container, opts) {
     if (overRenderer) {
       // cameraController.onMouseWheel(event);
       distanceTarget += event.wheelDeltaY * 0.3;
+      distanceTarget = Math.max( distanceTarget, 250.0 );
     }
     return false;
   }
